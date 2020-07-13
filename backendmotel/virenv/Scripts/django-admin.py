fa4e0352ae510f02c1f16fdb87f48dd41a52050b
tramanhk11uit <tramanh1998tls@gmail.com>
2020-07-13 20:07:12 +0700

    fix Register, login

---
+++ backendmotel/virenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\sourceTree\BackendMotel_Offical\backendmotel\virenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
